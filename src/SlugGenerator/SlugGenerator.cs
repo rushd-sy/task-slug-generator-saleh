3fab007c264546cfc7054dce0bb35d7515c68006
jalabi <mhmdmh2004@gmail.com>
2026-03-17 16:16:44 +0300

    feat/implement-class-library-logic

--- src/SlugGenerator/SlugGenerator.cs
+++ src/SlugGenerator/SlugGenerator.cs
@@ -4,6 +4,22 @@ public class SlugGenerator
 {
     public static string Generate(string text)
     {
-        throw new NotImplementedException();
+        string result = string.Empty;
+        text = text.ToLower();
+
+        foreach (char c in text)
+        {
+            if (char.IsLetter(c))
+                result = result + c;
+            else if ((c == '-' || c == '_' || c == ' ') && result.Length > 0)
+            {
+                if (result.Last() != '-' && result.Last() != '_' && result.Last() != ' ')
+                    result = result + "-";
+                else continue;
+            }
+            else continue;
+        }
+        result = result.TrimEnd('-');
+        return result;
     }
 }
