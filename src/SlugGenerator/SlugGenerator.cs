151bfd755bc7876feb74fda5e63d8963c7d5c243
jalabi <mhmdmh2004@gmail.com>
2026-03-18 17:18:36 +0300

    modify-class-library-logic

--- src/SlugGenerator/SlugGenerator.cs
+++ src/SlugGenerator/SlugGenerator.cs
@@ -1,25 +1,15 @@
-﻿namespace SlugGenerator;
+﻿using System.Text.RegularExpressions;
+
+namespace SlugGenerator;
 
 public class SlugGenerator
 {
     public static string Generate(string text)
     {
-        string result = string.Empty;
-        text = text.ToLower();
+        string result = text.ToLower();
+        result = string.Join("-", result.Split(new char[] { ' ', '-', '_' }, StringSplitOptions.RemoveEmptyEntries));
+        result = Regex.Replace(result, "[^a-z-ا-ي]", "");
 
-        foreach (char c in text)
-        {
-            if (char.IsLetter(c))
-                result = result + c;
-            else if ((c == '-' || c == '_' || c == ' ') && result.Length > 0)
-            {
-                if (result.Last() != '-' && result.Last() != '_' && result.Last() != ' ')
-                    result = result + "-";
-                else continue;
-            }
-            else continue;
-        }
-        result = result.TrimEnd('-');
         return result;
     }
 }
